1cceea4bccee6bcc8821e9799f9e47460939d37a
yhbin124 <yhbin124@g.skku.edu>
2025-11-19 03:13:18 +0900

    Update 아바결보.py

--- 아바결보.py
+++ 아바결보.py
@@ -109,12 +109,6 @@ if st.session_state.page == "E": #페이지 불러오기
 
 # --- NS ---
 elif st.session_state.page == "NS":
-    st.markdown("""
-    <script>
-        window.scrollTo({top: 0, behavior: 'auto'});
-    </script>
-""", unsafe_allow_html=True)
-
     st.title('page 2')
     for i, q in enumerate(questionsN, 1):
         response = st.radio(q, [1, 2, 3, 4, 5], horizontal=True, key=f"N{i}")
@@ -190,4 +184,5 @@ elif st.session_state.page == "RESULT":
     st.write(f"J = {ss4} : P = {100- ss4}")
 
     #최대점수 50 최저점수 10
+
 
